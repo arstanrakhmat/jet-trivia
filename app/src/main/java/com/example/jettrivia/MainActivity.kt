64e219ce88101388fd1882951fcfe06daf1cdc17
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-10-07 11:23:15 +0600

    commit of the done project

---
+++ app/src/main/java/com/example/jettrivia/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.jettrivia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.jettrivia.screens.TriviaHome
+import com.example.jettrivia.ui.theme.JetTriviaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetTriviaTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    TriviaHome()
+                }
+            }
+        }
+    }
+}
+
